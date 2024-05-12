7ca2ebd6ee2fc473a44dd595a0fcaea81f6a6ad5
KOSASIH <kosasihg88@gmail.com>
2024-05-12 13:18:17 +0700

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './store/reducers';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
