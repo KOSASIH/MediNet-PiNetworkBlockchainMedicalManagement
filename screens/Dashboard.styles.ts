b7a0aa42447711ab877fe1db9921111bd02eb7af
KOSASIH <kosasihg88@gmail.com>
2024-05-12 11:45:22 +0700

    Create Dashboard.styles.ts

---
+++ screens/Dashboard.styles.ts
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+}));
