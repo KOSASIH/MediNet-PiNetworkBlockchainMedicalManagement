2c13263b9c424f2e8d630d6089da8c83a42ede8b
KOSASIH <kosasihg88@gmail.com>
2024-05-12 11:56:23 +0700

    Create constants.ts

---
+++ utils/constants.ts
@@ -0,0 +1,4 @@
+export const BLOCKCHAIN_PORT = 3000;
+export const API_PORT = 4000;
+export const MEDICAL_RECORDS_API_ROUTE = '/api/medical-records';
+export const PATIENTS_API_ROUTE = '/api/patients';
