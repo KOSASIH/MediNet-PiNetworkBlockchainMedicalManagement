73775cde464dfb8acd5b4a5995967454d5c61de6
KOSASIH <kosasihg88@gmail.com>
2024-05-12 11:06:43 +0700

    Create environment.ts

---
+++ config/environment.ts
@@ -0,0 +1,10 @@
+// environment.ts
+export const environment = {
+  blockchain: {
+    networkId: 'mainnet',
+    contractAddress: '0x...',
+  },
+  api: {
+    url: 'https://api.medinet.com',
+  },
+};
