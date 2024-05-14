0eb774d66b4e9e2cc921d24204b5a2d3ec8bdfbd
KOSASIH <kosasihg88@gmail.com>
2024-05-14 19:16:51 +0700

    Create __init__.py

---
+++ medi_net/ai/__init__.py
@@ -0,0 +1,5 @@
+# medi_net/ai/__init__.py
+
+from .predictive_analytics import PredictiveAnalytics
+from .disease_diagnosis import DiseaseDiagnosis
+from .personalized_medicine import PersonalizedMedicine
